be178ba67d196cb66a1e9174896374e957df413a
TANISHKA KAMTHARIYA <144674434+Tanishkagit@users.noreply.github.com>
2024-01-23 21:42:37 +0530

    Create div.js

---
+++ div.js
@@ -0,0 +1,8 @@
+let box=document.querySelectorAll(".box");
+box[0].innerText="HELLO";
+box[1].innerText="HELLO";
+box[2].innerText="HELLO";
+let box1=document.querySelectorAll(".box1");
+box1[0].innerText="HELLO";
+box1[1].innerText="HELLO";
+box1[2].innerText="HELLO";
